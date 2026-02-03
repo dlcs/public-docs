c7838a34755738e50aee93a501a5ae63959a361a
tomcrane <tom@nanoplanet.com>
2026-02-03 17:46:22 +0000

    WIP - begin the usage section

--- src/astro.config.mjs
+++ src/astro.config.mjs
@@ -6,19 +6,25 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'IIIF Cloud Services',
+			logo: {
+				src: './src/assets/cloud-services.svg',
+				replacesTitle: true
+			},
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/dlcs/protagonist' }],
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+					label: 'Usage',					
+					autogenerate: { directory: 'usage' }
+					// items: [{ label: 'Example Guide', slug: 'guides/example' },],
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'The Portal',
+					autogenerate: { directory: 'portal' }
+				},
+				{
+					label: 'The API',
+					autogenerate: { directory: 'api-doc' },
 				},
 			],
 		}),
